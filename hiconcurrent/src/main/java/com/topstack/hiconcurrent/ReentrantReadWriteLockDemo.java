b4a211bdbc6e76fdf53c78e6d26dc03de1b8cde6
wangjincheng <wangjincheng@top-stack.com>
2022-07-20 21:39:51 +0800

    2.7.3_3-5:线程安全之共享锁,排他锁

---
+++ hiconcurrent/src/main/java/com/topstack/hiconcurrent/ReentrantReadWriteLockDemo.java
@@ -0,0 +1,74 @@
+package com.topstack.hiconcurrent;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 利用ReentrantReadWriteLock  来实现 多人在线文档查看与编辑的功能
+ *
+ * @author Administrator
+ */
+public class ReentrantReadWriteLockDemo {
+
+    public static void reentrantReadWriteLockTest() {
+
+        final ReentrantReadWriteLockTask task = new ReentrantReadWriteLockTask();
+
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    task.read();
+                }
+            }).start();
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    task.write();
+                }
+            }).start();
+        }
+    }
+
+    static class ReentrantReadWriteLockTask {
+        private final ReentrantReadWriteLock.ReadLock readLock;
+        private final ReentrantReadWriteLock.WriteLock writeLock;
+        ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+        ReentrantReadWriteLockTask() {
+            readLock = lock.readLock();
+            writeLock = lock.writeLock();
+        }
+
+        void read() {
+            String name = Thread.currentThread().getName();
+            try {
+                readLock.lock();
+                System.out.println("线程" + name + " 正在读取数据...");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                readLock.unlock();
+                System.out.println("线程" + name + " 释放了读锁...");
+            }
+        }
+
+        void write() {
+            String name = Thread.currentThread().getName();
+            try {
+                writeLock.lock();
+                System.out.println("线程" + name + " 正在写入数据...");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                writeLock.unlock();
+                System.out.println("线程" + name + " 释放了写锁...");
+            }
+        }
+    }
+}
